90620acb10b23fe2f830650601a1dbfbf2ae9b5a
Robeto Melo <rmello.filho@hotmail.com>
2024-04-12 09:11:39 -0300

    inicio

---
+++ CartaoShop/src/App.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import modelos.CartaoDeCredito;
+import modelos.Compra;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o limite do cartão: ");
+        double limite = leitura.nextDouble();
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+        int sair = 1;
+        while(sair != 0) {
+            System.out.println("Digite a descrição da compra:");
+            String descricao = leitura.next();
+
+            System.out.println("Digite o valor da compra:");
+            double valor = leitura.nextDouble();
+
+            Compra compra = new Compra(descricao, valor);
+            boolean compraRealizada = cartao.lancaCompra(compra);
+
+            if (compraRealizada) {
+                System.out.println("Compra realizada!");
+                System.out.println("Digite 0 para sair ou 1 para continuar");
+            } else {
+                System.out.println("Saldo insuficiente!");
+                sair = 0;
+            }
+        }
+
+        System.out.println("*****************");
+        System.out.println("COMPRAS REALIZADAS:\n");
+        Collections.sort(cartao.getCompras());
+        for (Compra c: cartao.getCompras()) {
+            System.out.println(c.getDescricao() + " - " + c.getValor());
+        }
+        System.out.println("\n***********************");
+
+        System.out.println("\nSaldo do cartão: " +cartao.getSaldo());
+        
+    }
+}
